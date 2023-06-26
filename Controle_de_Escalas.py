428fea99edd3dd8dfcdcc32b4c1dd6c9e8f62390
DanonePlayer <ce6519@gmail.com>
2023-06-26 13:04:58 -0500

    Update Controle_de_Escalas.py

--- Controle_de_Escalas.py
+++ Controle_de_Escalas.py
@@ -19,8 +19,13 @@ class Tela:
 
 
 
+        self.lbl_criar_escala = tk.Label(self.frm_cima, text="Criar Escala", font=("Arial",14), bg="#3CB371", fg="white", width=20, height=1)
+        self.lbl_criar_escala.place(x=10, y=10)
+        self.lbl_criar_escala.bind("<Button-1>", self.criar_escala) 
+
+
         self.lbl_novaprog = tk.Label(self.frm_cima, text="+ Nova programação", font=("Arial",14), bg="#3CB371", fg="white", width=20, height=1)
-        self.lbl_novaprog.place(x=10, y=10)
+        self.lbl_novaprog.place(x=10, y=50)
         self.lbl_novaprog.bind("<Button-1>", self.nova_tela)
 
         self.imgicon = tk.PhotoImage(file="a.png", height=222)
@@ -28,13 +33,10 @@ class Tela:
 
 
         self.lbl_verifica_escala = tk.Label(self.frm_cima, text="Verificar Escalas", font=("Arial",14), bg="#3CB371", fg="white", width=20, height=1)
-        self.lbl_verifica_escala.place(x=10, y=50)
+        self.lbl_verifica_escala.place(x=10, y=90)
         self.lbl_verifica_escala.bind("<Button-1>", self.verifica_escala)
 
 
-        self.lbl_criar_escala = tk.Label(self.frm_cima, text="Criar Escala", font=("Arial",14), bg="#3CB371", fg="white", width=20, height=1)
-        self.lbl_criar_escala.place(x=10, y=90)
-        self.lbl_criar_escala.bind("<Button-1>", self.criar_escala) 
 
 
 
@@ -48,6 +50,77 @@ class Tela:
 
         self.Tipo_p = []
 
+
+
+
+
+    def criar_escala(self, event):
+        self.janela_criar_escala = tk.Toplevel()
+        self.janela_criar_escala.title("Criar Escala")
+        self.janela_criar_escala.grab_set()
+        self.janela_criar_escala.geometry("600x400")
+
+
+        self.lbl_nome_escala = tk.Label(self.janela_criar_escala, text="Nome da Escala:")
+        self.lbl_nome_escala.place(x=10, y=10)
+
+        self.entry_nome_escala = tk.Entry(self.janela_criar_escala, borderwidth=2)
+        self.entry_nome_escala.place(x=10, y=30)
+
+
+
+        self.lbl_dias = tk.Label(self.janela_criar_escala, text="Quantos dias:")
+        self.lbl_dias.place(x=10, y=70)
+
+        vetor_num = []
+        for nums in range(1, 360):
+            vetor_num.append(nums)
+
+        self.string_Var_comb = tk.StringVar()
+        self.comb = ttk.Combobox(self.janela_criar_escala, textvariable=self.string_Var_comb)
+        self.comb["values"] = vetor_num
+        self.comb.place(x=10, y=90)
+
+        self.int_var_sem = tk.IntVar()
+        self.int_var_fer = tk.IntVar()
+
+        self.lbl_cont_final_semana = tk.Label(self.janela_criar_escala, text="Contará os finais de semana?")
+        self.lbl_cont_final_semana.place(x=10, y=130)
+
+        self.rbtn_cont_final_semanas = tk.Radiobutton(self.janela_criar_escala, text="Sim", variable=self.int_var_sem, value=1)
+        self.rbtn_cont_final_semanas.place(x=10, y=150)
+        
+        self.rbtn_cont_final_semanan = tk.Radiobutton(self.janela_criar_escala, text="Não", variable=self.int_var_sem, value=2)
+        self.rbtn_cont_final_semanan.place(x=90, y=150)
+
+
+
+        self.lbl_cont_ferias = tk.Label(self.janela_criar_escala, text="Contará as Férias?")
+        self.lbl_cont_ferias.place(x=10, y=190)
+
+        self.rbtn_cont_feriass= tk.Radiobutton(self.janela_criar_escala, text="Sim", variable=self.int_var_fer, value=1)
+        self.rbtn_cont_feriass.place(x=10, y=210)
+        
+        self.rbtn_cont_feriasn = tk.Radiobutton(self.janela_criar_escala, text="Não", variable=self.int_var_fer, value=2)
+        self.rbtn_cont_feriasn.place(x=90, y=210)
+
+
+        self.btn_ok = tk.Button(self.janela_criar_escala, text='Criar', command=self.button_criar_escala)
+        self.btn_ok.place(x=100, y=350)
+
+
+    def button_criar_escala(self):
+        if self.Tipo_p != []:
+            if (f"{self.entry_nome_escala.get()} - {self.string_Var_comb.get()}") in self.Tipo_p:
+                print("replica")
+                self.janela_criar_escala.destroy()
+            else:
+
+                self.Tipo_p.append(f"{self.entry_nome_escala.get()} - {self.string_Var_comb.get()}")
+                self.janela_criar_escala.destroy()
+        else:
+            self.Tipo_p.append(f"{self.entry_nome_escala.get()} - {self.string_Var_comb.get()}")
+            self.janela_criar_escala.destroy()
 
 
 
@@ -178,73 +251,7 @@ class Tela:
         cal.pack(fill="both", expand=True)
 
 
-    def criar_escala(self, event):
-        self.janela_criar_escala = tk.Toplevel()
-        self.janela_criar_escala.title("Criar Escala")
-        self.janela_criar_escala.grab_set()
-        self.janela_criar_escala.geometry("600x400")
 
-
-        self.lbl_nome_escala = tk.Label(self.janela_criar_escala, text="Nome da Escala:")
-        self.lbl_nome_escala.place(x=10, y=10)
-
-        self.entry_nome_escala = tk.Entry(self.janela_criar_escala, borderwidth=2)
-        self.entry_nome_escala.place(x=10, y=30)
-
-
-
-        self.lbl_dias = tk.Label(self.janela_criar_escala, text="Quantos dias:")
-        self.lbl_dias.place(x=10, y=70)
-
-        vetor_num = []
-        for nums in range(1, 360):
-            vetor_num.append(nums)
-
-        self.string_Var_comb = tk.StringVar()
-        self.comb = ttk.Combobox(self.janela_criar_escala, textvariable=self.string_Var_comb)
-        self.comb["values"] = vetor_num
-        self.comb.place(x=10, y=90)
-
-        self.int_var_sem = tk.IntVar()
-        self.int_var_fer = tk.IntVar()
-
-        self.lbl_cont_final_semana = tk.Label(self.janela_criar_escala, text="Contará os finais de semana?")
-        self.lbl_cont_final_semana.place(x=10, y=130)
-
-        self.rbtn_cont_final_semanas = tk.Radiobutton(self.janela_criar_escala, text="Sim", variable=self.int_var_sem, value=1)
-        self.rbtn_cont_final_semanas.place(x=10, y=150)
-        
-        self.rbtn_cont_final_semanan = tk.Radiobutton(self.janela_criar_escala, text="Não", variable=self.int_var_sem, value=2)
-        self.rbtn_cont_final_semanan.place(x=90, y=150)
-
-
-
-        self.lbl_cont_ferias = tk.Label(self.janela_criar_escala, text="Contará as Férias?")
-        self.lbl_cont_ferias.place(x=10, y=190)
-
-        self.rbtn_cont_feriass= tk.Radiobutton(self.janela_criar_escala, text="Sim", variable=self.int_var_fer, value=1)
-        self.rbtn_cont_feriass.place(x=10, y=210)
-        
-        self.rbtn_cont_feriasn = tk.Radiobutton(self.janela_criar_escala, text="Não", variable=self.int_var_fer, value=2)
-        self.rbtn_cont_feriasn.place(x=90, y=210)
-
-
-        self.btn_ok = tk.Button(self.janela_criar_escala, text='Criar', command=self.button_criar_escala)
-        self.btn_ok.place(x=100, y=350)
-
-
-    def button_criar_escala(self):
-        if self.Tipo_p != []:
-            if (f"{self.entry_nome_escala.get()} - {self.string_Var_comb.get()}") in self.Tipo_p:
-                print("replica")
-                self.janela_criar_escala.destroy()
-            else:
-
-                self.Tipo_p.append(f"{self.entry_nome_escala.get()} - {self.string_Var_comb.get()}")
-                self.janela_criar_escala.destroy()
-        else:
-            self.Tipo_p.append(f"{self.entry_nome_escala.get()} - {self.string_Var_comb.get()}")
-            self.janela_criar_escala.destroy()
 
 
 janela = tk.Tk()
